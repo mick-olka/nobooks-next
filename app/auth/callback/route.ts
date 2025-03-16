f457a1cf59b88806a77e7863ea0d9417d6dcaa66
mick-olka <mickolka64@gmail.com>
2025-03-16 22:38:25 +0000

    test

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,37 +3,30 @@ import { NextResponse } from "next/server";
 import { createClient } from "@/app/utils/supabase/server";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
+  const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/";
-  console.log(code);
+
   if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
-      const forwardedProto = request.headers.get("x-forwarded-proto"); // protocol (http/https)
       const isLocalEnv = process.env.NODE_ENV === "development";
-
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(
-          `${process.env.NEXT_PUBLIC_APP_URL}${next}`
-        );
+        return NextResponse.redirect(`${origin}${next}`);
+        // biome-ignore lint/style/noUselessElse: <explanation>
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+        // biome-ignore lint/style/noUselessElse: <explanation>
+      } else {
+        return NextResponse.redirect(`${origin}${next}`);
       }
-
-      if (forwardedHost) {
-        const protocol = forwardedProto || "https";
-        return NextResponse.redirect(`${protocol}://${forwardedHost}${next}`);
-      }
-
-      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}${next}`);
     }
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(
-    `${process.env.NEXT_PUBLIC_APP_URL}/auth/auth-code-error`
-  );
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
