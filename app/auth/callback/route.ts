a7eb14bd4b04e1b6103e5002f54456b7e621fe39
mick-olka <mickolka64@gmail.com>
2025-03-16 23:25:01 +0000

    test

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,37 +4,29 @@ import { createClient } from "@/app/utils/supabase/server";
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
-  console.log(searchParams, origin);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
-  // const next = searchParams.get("next") ?? "/";
-  console.log(code);
+  const next = searchParams.get("next") ?? "/";
+
   if (code) {
     const supabase = await createClient();
-    const { error, data } = await supabase.auth.exchangeCodeForSession(code);
-    console.log(data, error);
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
-      // const isLocalEnv = process.env.NODE_ENV === "development";
-      console.log(
-        "All headers:",
-        Object.fromEntries(request.headers.entries())
-      );
-      console.log("Forwarded host:", forwardedHost);
-      // if (isLocalEnv) {
-      //   // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-      //   return NextResponse.redirect(`${origin}${next}`);
-      //   // biome-ignore lint/style/noUselessElse: <explanation>
-      // } else if (forwardedHost) {
-      //   return NextResponse.redirect(`https://${forwardedHost}${next}`);
-      //   // biome-ignore lint/style/noUselessElse: <explanation>
-      // } else {
-      //   return NextResponse.redirect(`${origin}${next}`);
-      // }
-      return NextResponse.redirect("https://noboobs.world");
+      const isLocalEnv = process.env.NODE_ENV === "development";
+      if (isLocalEnv) {
+        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+        return NextResponse.redirect(`${origin}${next}`);
+        // biome-ignore lint/style/noUselessElse: <explanation>
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+        // biome-ignore lint/style/noUselessElse: <explanation>
+      } else {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
     }
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/error`);
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
