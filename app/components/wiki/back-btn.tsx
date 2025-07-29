ffc70d7972fe2206dba81f906f97bea781059536
mick-olka <mickolka64@gmail.com>
2025-07-29 13:52:14 +0100

    upd: back btn

--- app/components/wiki/back-btn.tsx
+++ app/components/wiki/back-btn.tsx
@@ -6,13 +6,13 @@ export const BackBtn = ({ isAdmin }: { isAdmin?: boolean }) => {
 	const router = useRouter();
 
 	const handleClick = () => {
-		if (window.history.length > 2) {
-			if (isAdmin) {
-				router.back();
-			} else router.back();
-		} else {
-			router.push("/");
-		}
+		// if (window.history.length > 2) {
+		// 	if (isAdmin) {
+		// 		router.back();
+		// 	} else router.back();
+		// } else {
+		router.push("/wiki");
+		// }
 	};
 
 	return (
