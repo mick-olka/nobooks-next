87898095494ebed261d6b709480fa0bd4dd26e58
mick-olka <mickolka64@gmail.com>
2026-01-17 14:55:37 +0200

    upd: map url

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,6 +1,7 @@
 export const constants = {
   MAP_URL:
-    "https://map.noboobs.world:3140/#world:32:0:48:1500:0:0:0:0:perspective",
+  "https://map.noboobs.world",
+    // "https://map.noboobs.world:3140/#world:32:0:48:1500:0:0:0:0:perspective",
   JAVA_IP: "mc.noboobs.world",
   BEDROCK_IP: "bedrock.noboobs.world:1423",
   SITE_URL: "https://noboobs.world",
